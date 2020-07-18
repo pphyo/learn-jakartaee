8336219f49edaf0893047681601a4b92c67d4c3a
P Phyo <sharloghome808@gmail.com>
2020-07-19 02:12:05 +0630

    Intro to JSP

---
+++ hello-jsp/src/main/java/com/jdc/communication/HeaderServlet.java
@@ -0,0 +1,26 @@
+package com.jdc.communication;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "header", urlPatterns = "/header")
+public class HeaderServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter()
+			.append("<html><head><title>")
+			.append("Include Servlet")
+			.append("</title></head>")
+			.append("<body>")
+			.flush();
+	}
+
+}
---
+++ hello-jsp/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Hello JSP</title>
+</head>
+<body>
+	<h1>Hello JSP</h1>
+	
+	<ul>
+		<li><a href="include">Include</a></li>
+		<li><a href="forward">Forward</a></li>
+		<li><a href="forward-jsp">Forward to JSP</a></li>
+	</ul>
+	
+</body>
+</html>
